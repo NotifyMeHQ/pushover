feed748160c5bb3d369847ae83d4a3142e0c6c40
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-03-13 17:04:33 -0600

    Update to the new interface

--- src/PushoverGateway.php
+++ src/PushoverGateway.php
@@ -97,13 +97,16 @@ class PushoverGateway implements GatewayInterface
     /**
      * Send a notification.
      *
+     * @param string   $to
      * @param string   $message
      * @param string[] $options
      *
      * @return \NotifyMeHQ\NotifyMe\Response
      */
-    public function notify($message, array $options = [])
+    public function notify($to, $message, array $options = [])
     {
+        $options['to'] = $to;
+        
         $params = $this->addMessage($message, $params, $options);
 
         return $this->commit('post', $this->buildUrlFromString('messages.json'), $params);
